24807a7a7fc8d3cbf35ec6ee4f77a17505ddde36
Douglas Fanucchi <douglasfanucchi@gmail.com>
2025-05-13 14:27:30 -0300

    first commit

---
+++ pagbank-split-payment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     PagBank Split Payment
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     pagbank-split-payment
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         PagBank_Split_Payment
+ */
+
+defined( 'ABSPATH' ) || exit;
